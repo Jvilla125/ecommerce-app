e76dab9b5b388e7e0b968a7e1c1595e65e77e2fa
jeffvilla <jvilla125@gmail.com>
2023-06-19 11:52:02 -0700

    Create routes to homepage in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 
 function App() {
   return (
-   <>
-   <HomePage/>
-   </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
