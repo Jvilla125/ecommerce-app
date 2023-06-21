f800086b2fc32da1bfdd553c799b6c4abe2bf04f
jeffvilla <jvilla125@gmail.com>
2023-06-21 11:43:30 -0700

    import Link from react-router-dom to link pages so user does not have to reload the page in HeaderComponent.js

--- src/pages/components/HeaderComponent.js
+++ src/pages/components/HeaderComponent.js
@@ -1,44 +1,64 @@
 import React from 'react';
-import { Navbar, Nav, Container, NavDropdown, Badge, Form, Dropdown, DropdownButton, Button, InputGroup} from 'react-bootstrap';
-
+import { Navbar, Nav, Container, NavDropdown, Badge, Form, Dropdown, DropdownButton, Button, InputGroup } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap'
+import { Link } from 'react-router-dom';
 
 const HeaderComponent = () => {
     return (
         <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                {/* Link container allows user to go to link without reloading the page */}
+                <LinkContainer to="/">
+                    <Navbar.Brand href="/">Best Online Shop</Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
                         <InputGroup>
-                        <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                        </DropdownButton>
-                        <Form.Control type="text" placeholder="Normal text" />
-                        <Button variant="warning">Warning</Button>
+                            <DropdownButton id="dropdown-basic-button" title="All">
+                                <Dropdown.Item >Electronics</Dropdown.Item>
+                                <Dropdown.Item >Cars</Dropdown.Item>
+                                <Dropdown.Item >Books</Dropdown.Item>
+
+                            </DropdownButton>
+                            <Form.Control type="text" placeholder="Search in shop ..." />
+                            <Button variant="warning">
+                                <i className="bi bi-search text-dark"></i>
+                            </Button>
                         </InputGroup>
 
-                        </Nav>
-                        <Nav>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                        <Nav.Link href="#pricing"><Badge bg="danger">2</Badge>Cart</Nav.Link>
-
-                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
                     </Nav>
-
+                    <Nav>
+                        <LinkContainer to="/admin/orders">
+                            <Nav.Link >Admin
+                                {/* the span will notify the user if there is a chat available */}
+                                <span className="position-absolute top-1 start-10 translate-middle p-2 bg-danger border border-light rounded-circle"></span>
+                            </Nav.Link>
+                        </LinkContainer>
+                        <NavDropdown title="User" id="collasible-nav-dropdown">
+                            {/* eventKey lets users know that we are currently on that page by highlighting it */}
+                            <NavDropdown.Item eventKey="/user/my-orders" as={Link} to="/user/my-orders">My orders</NavDropdown.Item>
+                            <NavDropdown.Item eventKey="/user/" as={Link} to="/user">My Profile</NavDropdown.Item>
+                            <NavDropdown.Item >Logout</NavDropdown.Item>
+                        </NavDropdown>
+                        <LinkContainer to="/login">
+                            <Nav.Link >
+                                Login
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/register">
+                            <Nav.Link >
+                                Register
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/cart">
+                            <Nav.Link >
+                            <Badge bg="danger">2</Badge>
+                            <i className="bi bi-cart-dash"></i>
+                            <span className='ms-1'> Cart </span>
+                            </Nav.Link>
+                        </LinkContainer>
+                    </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
