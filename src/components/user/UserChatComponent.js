11fe4180a2d8d126a6bc9da5ec00d9d85022f2eb
jeffvilla <jvilla125@gmail.com>
2023-06-21 14:25:17 -0700

    Add a div in UserChatComponent for the chat box. Add style to chat.css chatbox div to appear when check and disappear when unchecked

--- src/components/user/UserChatComponent.js
+++ src/components/user/UserChatComponent.js
@@ -10,6 +10,11 @@ const UserChatComponent = () => {
                 <i className="bi bi-chat-dots comment"></i>
                 <i className="bi bi-x-circle close"></i>
             </label>
+            <div className='chat-wrapper'>
+                <div className='chat-header'>
+                    <h6>Let's Chat - Online</h6>
+                </div>
+            </div>
         </>
     )
 }
