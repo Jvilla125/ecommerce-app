8c07be6e0374654061904427c43c1e238a41e7cf
jeffvilla <jvilla125@gmail.com>
2023-06-21 11:54:54 -0700

    Add footer and fluid container to FooterComponent.js to style it. Also added style to index.css to have footer stick at the bottom of the window page

--- src/pages/components/FooterComponent.js
+++ src/pages/components/FooterComponent.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap'
 
 const FooterComponent = () => {
-    return <p>This is a footer</p>
+    return (
+        <footer >
+            <Container fluid> 
+                <Row className="mt-5">
+                    <Col className='text-white bg-dark text-center py-5'>
+                        Copyright &copy; Best Online Shop
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
 }
 
 export default FooterComponent;
