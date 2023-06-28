88b0ac6d3b3806f1e516e53cfc33f143d56855c1
jeffvilla <jvilla125@gmail.com>
2023-06-28 13:48:37 -0700

    Import a table for UserOrderPage.js

--- src/pages/user/UserOrdersPage.js
+++ src/pages/user/UserOrdersPage.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { Row, Col, Table } from 'react-bootstrap';
+import { Link } from 'react-router-dom'
 
-const UserOrdersPage = () =>{
-    return<p>User Orders page!</p>
+const UserOrdersPage = () => {
+    return (
+        <Row className="m-5">
+            <Col md={12}>
+                <h1>My orders</h1>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>User</th>
+                            <th>Date</th>
+                            <th>Total</th>
+                            <th>Delivered</th>
+                            <th>Order Details</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {["bi bi-check-lg text-success", "bi bi-x-lg text-danger"].map((item, idx) => (
+                            <tr key={idx}>
+                                <td>{idx + 1} </td>
+                                <td>Mark Twain</td>
+                                <td>2022-09-12</td>
+                                <td>$123</td>
+                                <td>
+                                    <i className={item}></i>
+                                </td>
+                                <td><Link to="/user/order-details"> Go to order</Link></td>
+                            </tr>
+                        ))}
+
+
+                    </tbody>
+                </Table>
+            </Col>
+
+        </Row>
+    )
 }
 
 export default UserOrdersPage;
--- src/pages/user/UserProfilePage.js
+++ src/pages/user/UserProfilePage.js
@@ -1,7 +1,146 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Container, Col, Row, Form, Button, Alert } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-const UserProfilePage = () =>{
-    return<p>User profile page!</p>
+const UserProfilePage = () => {
+    const [validated, setValidated] = useState(false);
+
+    const onChange = () => {
+        const password = document.querySelector('input[name=password]')
+        const confirm = document.querySelector('input[name=confirmPassword]')
+        if (confirm.value === password.value) {
+            confirm.setCustomValidity("")
+        } else {
+            confirm.setCustomValidity("Passwords do not match")
+        }
+    }
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        setValidated(true);
+    };
+
+    return (
+        <Container >
+            <Row className='mt-5 justify-content-md-center' >
+                <Col md={6}>
+                    <h1>User Profile</h1>
+                    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                        <Form.Group className='mb-3' controlId="validationCustom01">
+                            <Form.Label>Your name</Form.Label>
+                            <Form.Control
+                                required
+                                type="text"
+                                defaultValue="John"
+                                name="name"
+                            />
+                            <Form.Control.Feedback type="invalid">Please enter a name</Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group className='mb-3' controlId="formBasicLastName">
+                            <Form.Label>Your Last Name</Form.Label>
+                            <Form.Control
+                                required
+                                type="text"
+                                defaultValue="Doe"
+                                name="lastName"
+                            />
+                            <Form.Control.Feedback type="invalid"> Please enter your last name</Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group className='mb-3' controlId="formBasicEmail">
+                            <Form.Label>Email Address</Form.Label>
+                            <Form.Control
+                                disabled
+                                required
+                                value="if you want to change email, please remove account and create a new one"
+                            />
+                        </Form.Group>
+                        <Form.Group className='mb-3' controlId="formBasicPhone">
+                            <Form.Label> Phone number</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter your phone number"
+                                defaultValue=""
+                            />
+                        </Form.Group>
+                        <Form.Group className='mb-3' controlId="formBasicAddress">
+                            <Form.Label> Address </Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter your street name and house number"
+                                defaultValue=""
+                            />
+                        </Form.Group>
+                        <Form.Group className='mb-3' controlId="formBasicCountry">
+                            <Form.Label> Country </Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter your country"
+                                defaultValue=""
+                            />
+                        </Form.Group>
+                        <Form.Group className='mb-3' controlId="formBasicZip">
+                            <Form.Label> Zip Code </Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter your Zip code"
+                                defaultValue=""
+                            />
+                        </Form.Group>
+                        <Form.Group className='mb-3' controlId="formBasicCity">
+                            <Form.Label> City </Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter your city"
+                                defaultValue=""
+                            />
+                        </Form.Group>
+                        <Form.Group className='mb-3' controlId="formBasicState">
+                            <Form.Label> State </Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter your state"
+                                defaultValue=""
+                            />
+                        </Form.Group>
+                        <Form.Group className='mb-3' controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                required
+                                type="password"
+                                placeholder="Password"
+                                name="password"
+                                minLength={6}
+                                onChange={onChange}
+                            />
+                            <Form.Control.Feedback type="invalid"> Please enter a valid password</Form.Control.Feedback>
+                            <Form.Text className='text-muted'>Password should have at least 6 characters</Form.Text>
+                        </Form.Group>
+                        <Form.Group className='mb-3' controlId="formBasicPasswordRepeat">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                required
+                                type="password"
+                                placeholder="Repeat Password"
+                                name="confirmPassword"
+                                minLength={6}
+                                onChange={onChange}
+                            />
+                            <Form.Control.Feedback type="invalid"> Both passwords should match</Form.Control.Feedback>
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Update
+                        </Button>
+                        <Alert show={true} variant="danger"> User with that email already exists!</Alert>
+                        <Alert show={true} variant="info"> User updated!</Alert>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    )
 }
 
 export default UserProfilePage;
