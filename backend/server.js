b26658ef9f561343f111480bfe399fca7d5b41c5
jeffvilla <jvilla125@gmail.com>
2023-06-29 12:34:47 -0700

    successfully connect to Mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
+
 const express = require('express')
 const app = express()
 const port = 3000
-
 
 const apiRoutes = require('./routes/apiRoutes');
 
@@ -10,24 +10,11 @@ const apiRoutes = require('./routes/apiRoutes');
 // app.get takes two arguments, "/" routing and (req,res) that handles the path
 // request and response
 app.get('/', (req, res) => {
-    console.log("synchronous code")
-    throw new Error("some error occured")
     res.json({message: "API running..."})
 })
-
-app.get("/a", (req,res,next) => {
-    // Building Asynchronous code
-    // Server goes to next line of code, doesn't wait for setTimeout() function to finish
-    setTimeout(() => {
-        // try catch is used to catch errors
-        try {
-            aconsole.log("asynchronous code");
-        } catch (err){
-            next(err);
-        }
-    }, 1000)
-    // next(new Error("some error occured"))
-})
+// mongodb connection
+const connectDB = require('./config/db')
+connectDB();
 
 // if URL starts with /api, then it is handled with apiRoutes.js
 app.use('/api', apiRoutes)
