5178f24aca1e82e3b0c1aa307463aa1833d3c24b
jeffvilla <jvilla125@gmail.com>
2023-07-05 22:04:49 -0700

    Add 3 routes in apiRoutes.js

--- backend/routes/apiRoutes.js
+++ backend/routes/apiRoutes.js
@@ -5,7 +5,9 @@ const productRoutes = require('./productRoutes')
 
 // from server.js if the URL first part is /api and second is /products
 // /api/products, then it is handled to productRoutes.js
-
 app.use('/products', productRoutes);
+app.use('/categories', categoryRoutes);
+app.use('/users', userRoutes);
+app.use('/orders', orderRoutes);
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
 const apiRoutes = require('./routes/apiRoutes');
 
 // mongodb connection
