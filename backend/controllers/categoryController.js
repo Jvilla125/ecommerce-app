f22cd956457cd1828c8ce4926fe7de962d387a8b
jeffvilla <jvilla125@gmail.com>
2023-07-05 22:30:14 -0700

    Create success api endpoint for categoryController.js. It returns json to localhost.3000/api/categories

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -1,7 +1,20 @@
 // Controller methods will be performing actual database operations for our app
 
-const getCategories = (req,res) => {
-    res.send("Handling category routes, e.g. search for categories")
+// Need to import the Category model below VV
+const Category = require('../models/CategoryModel');
+
+const getCategories = async (req, res, next) => {
+    try {
+        // use Category.find to all categories from the database
+        // inside the brackets we can filter by name,price, etc.
+        const categories = await Category.find({}).sort({ name: "asc" }).orFail()
+        // .orFail() means that if there are no categories in the Database, an error will be thrown
+        //response is the json of categories
+        res.json(categories)
+        
+    } catch (error) {
+        next(error)
+    }
 }
 
 module.exports = getCategories
