6c2dbd070f80b9a6462e3fd3e3913ad989bf37db
jeffvilla <jvilla125@gmail.com>
2023-07-06 14:21:47 -0700

    Add getBestSellers route to productRoutes.js and create an async and await function to get the top 3 best sellers from each category

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -135,4 +135,34 @@ const getProducts = async (req, res, next) => {
     }
 }
 
-module.exports = getProducts
+const getProductsById = async (req, res, next) => {
+    try {
+        // req.params.id is from productRoutes.js line 11 "/:id"
+        const product = await Product.findById(req.params.id).populate("reviews").orFail() // populate("reviews") shows all reviews content
+        res.json(product)
+    } catch (error) {
+        next(error)
+    }
+}
+
+const getBestSellers = async (req, res, next) => {
+    try {
+        const products = await Product.aggregate([
+            { $sort: { category: 1, sales: -1 } },
+            { $group: { _id: "$category", doc_with_max_sales: { $first: "$$ROOT" } } },
+            { $replaceWith: "$doc_with_max_sales" },
+            { $match: { sales: { $gt: 0 } } },
+            { $project: { _id: 1, name: 1, images: 1, category: 1, description: 1 } }, // only get the following requested
+            { $limit: 3 }
+        ])
+        res.json(products)
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports = {
+    getProducts,
+    getProductsById,
+    getBestSellers
+}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const getProducts = require("../controllers/productController")
+const { getProducts, getProductsById, getBestSellers } = require("../controllers/productController")
 
 // The URL is /api/products and the response to the user will be the following:
 // handler in the routes will be handled in controller (separate file)
@@ -8,5 +8,7 @@ router.get("/category/:categoryName/search/:searchQuery", getProducts);
 router.get("/category/:categoryName", getProducts);
 router.get("/search/:searchQuery", getProducts);
 router.get("/", getProducts);
+router.get("/bestsellers", getBestSellers)
+router.get("/:id", getProductsById)
 
 module.exports = router
