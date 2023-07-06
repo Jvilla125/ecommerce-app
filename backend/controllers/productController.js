33df1ab31f18d1c603bcf3cf2695045fb76383af
jeffvilla <jvilla125@gmail.com>
2023-07-06 12:11:06 -0700

    Create search query for categories in productController.js

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,8 +3,41 @@ const recordsPerPage = require('../config/pagination');
 
 const getProducts = async (req, res, next) => {
     try {
+
+        let query = {}
+        let queryCondition = false;
+
+        let priceQueryCondition = {}
+        if (req.query.price) {
+            queryCondition = true;
+            // $lte mongodb syntax means less than
+            priceQueryCondition = { price: { $lte: Number(req.query.price) } }
+        }
+
+        let ratingQueryCondition = {}
+        if (req.query.rating) {
+            queryCondition = true;
+            // $in mongodb syntax means rating includes 
+            ratingQueryCondition = { rating: { $in: req.query.rating.split(",") } }
+        }
+
+        let categoryQueryCondition = {}
+        const categoryName = req.params.categoryName || "";
+        if(categoryName){
+            queryCondition = true;
+            let a = categoryName.replaceAll(',', "/")
+            var regEx = new RegExp("^" + a)
+            categoryQueryCondition = {category: regEx}
+        }
+
+        if (queryCondition){
+            query = {
+                $and: [priceQueryCondition, ratingQueryCondition, categoryQueryCondition]
+            }
+        }
+        
+
         const pageNum = Number(req.query.pageNum) || 1;
-        const totalProducts = await Product.countDocuments({})
 
         // sort by name, price, customer rating
         // sorting based on //front/src/components/SortOptionsComponent.js
@@ -23,7 +56,12 @@ const getProducts = async (req, res, next) => {
         // find all products by using .find({})
         // .sort({name: 1}) means products will be sorted in ascending order 
         // .limit(1) reduces the amount of products being returned
-        const products = await Product.find({}).skip(recordsPerPage * (pageNum - 1)).sort(sort).limit(recordsPerPage)
+
+        const totalProducts = await Product.countDocuments(query)
+        const products = await Product.find(query)
+            .skip(recordsPerPage * (pageNum - 1))
+            .sort(sort)
+            .limit(recordsPerPage)
 
         res.json({
             products,
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,6 +4,7 @@ const getProducts = require("../controllers/productController")
 
 // The URL is /api/products and the response to the user will be the following:
 // handler in the routes will be handled in controller (separate file)
+router.get("/category/:categoryName", getProducts)
 router.get("/", getProducts);
 
 module.exports = router
