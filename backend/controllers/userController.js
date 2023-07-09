c2df159748477bc9c7b9c14925aa39fbb2abc802
jeffvilla <jvilla125@gmail.com>
2023-07-08 22:56:03 -0700

    Create registerUser function in userController.js file. If user submits without inputting all field then there will be a 400 user error returned.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,7 @@ const User = require('../models/UserModel');
 
 const getUsers = async (req, res, next) => {
     try {
-        const users = await User.find({}).select("-password") 
+        const users = await User.find({}).select("-password")
         return res.json(users)
         //fetch all users from database ({})
         // .select("-password") excludes what will be sent in the json 
@@ -12,4 +12,34 @@ const getUsers = async (req, res, next) => {
     }
 }
 
-module.exports = getUsers
+const registerUser = async (req, res, next) => {
+    try {
+        const { name, lastName, email, password } = req.body
+        if (!(name && lastName && email && password)) { // if any of the input fields are missing
+            // it will be a 400 (user) error 
+            return res.status(400).send("All inputs are required")
+        }
+        // find a user by looking up their email in the database
+        const userExists = await User.findOne({ email })
+        // if userExist is true -> return user error json response with user exists
+        // if userExist is false -> create new user
+        if (userExists) {
+            return res.status(400).json({ error: "user exists" })
+        } else {
+            const user = await User.create({
+                name,
+                lastName,
+                email: email.toLowerCase(),
+                password: password
+            })
+            res.status(201).send(user) //if user was created successfully, it will return a success message
+        }
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports = {
+    getUsers,
+    registerUser,
+}
