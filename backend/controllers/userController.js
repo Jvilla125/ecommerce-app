8d389e42e08e388407d808e5837c9697c175acd4
jeffvilla <jvilla125@gmail.com>
2023-07-08 22:39:23 -0700

    Update userController.js getUsers function to find all users from database while excluding '-password' from the response

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,15 @@
 // Controller methods will be performing actual database operations for our app
+const User = require('../models/UserModel');
 
-const getUsers = (req,res) => {
-    res.send("Handling user routes, e.g. search for users")
+const getUsers = async (req, res, next) => {
+    try {
+        const users = await User.find({}).select("-password") 
+        return res.json(users)
+        //fetch all users from database ({})
+        // .select("-password") excludes what will be sent in the json 
+    } catch (error) {
+        next(error)
+    }
 }
 
 module.exports = getUsers
