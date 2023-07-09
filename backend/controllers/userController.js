5a5f67140bf394c54d94d42896bab1ae130821f6
jeffvilla <jvilla125@gmail.com>
2023-07-08 23:37:43 -0700

    create a cookie in registerUser function to keep of a user when they are successfully registered

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -20,13 +20,13 @@ const registerUser = async (req, res, next) => {
             // it will be a 400 (user) error 
             return res.status(400).send("All inputs are required")
         }
-        // find a user by looking up their email in the database
-        const userExists = await User.findOne({ email })
+
+        const userExists = await User.findOne({ email }) // find a user by looking up their email in the database
+
         // if userExist is true -> return user error json response with user exists
-        // if userExist is false -> create new user
         if (userExists) {
-            return res.status(400).json({ error: "user exists" })
-        } else {
+            return res.status(400).send("user exists");
+        } else { // if userExist is false -> create new user
             const hashedPassword = hashPassword(password) // <- password comes from req.body
             const user = await User.create({
                 name,
@@ -34,7 +34,24 @@ const registerUser = async (req, res, next) => {
                 email: email.toLowerCase(),
                 password: hashedPassword // <- imported from utils/hashPassword file to be more secure
             })
-            res.status(201).send(user) //if user was created successfully, it will return a success message
+
+            res
+            .cookie("access_token", "fake access token", { // <- cookie will be created on the client side when a user is successfully created
+                httpOnly: true, // <- the cookie can only be accessed using http protocol
+                secure: process.env.NODE_ENV === "production",
+                sameSite: "strict" 
+            }) 
+                .status(201)
+                .json({ // When a user is created, the following will be shown and password will not be shown
+                    success: "User created",
+                    userCreated: {
+                        _id: user._id,
+                        name: user.name,
+                        lastName: user.lastName,
+                        email: user.email,
+                        isAdmin: user.isAdmin,
+                    },
+                })
         }
     } catch (error) {
         next(error)
