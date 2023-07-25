4e01e98402e2569f77cc7acf3b7bf0f5ee56457f
jeffvilla <jvilla125@gmail.com>
2023-07-24 22:13:01 -0700

    dynamically import user orders information

--- frontend/src/pages/user/UserOrdersPage.js
+++ frontend/src/pages/user/UserOrdersPage.js
@@ -1,40 +1,18 @@
-import React from 'react';
-import { Row, Col, Table } from 'react-bootstrap';
-import { Link } from 'react-router-dom'
+import UserOrdersPageComponent from "./components/UserOrdersPageComponent";
+
+import axios from "axios";
+
+const getOrders = async () => {
+    const { data } = await axios.get("/api/orders");
+    return data;
+}
+
 
 const UserOrdersPage = () => {
     return (
-        <Row className="m-5">
-            <Col md={12}>
-                <h1>My orders</h1>
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>User</th>
-                            <th>Date</th>
-                            <th>Total</th>
-                            <th>Delivered</th>
-                            <th>Order Details</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {["bi bi-check-lg text-success", "bi bi-x-lg text-danger"].map((item, idx) => (
-                            <tr key={idx}>
-                                <td>{idx + 1} </td>
-                                <td>Mark Twain</td>
-                                <td>2022-09-12</td>
-                                <td>$123</td>
-                                <td>
-                                    <i className={item}></i>
-                                </td>
-                                <td><Link to="/user/order-details"> Go to order</Link></td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </Table>
-            </Col>
-        </Row>
+        <UserOrdersPageComponent
+            getOrders={getOrders}
+        />
     )
 }
 
