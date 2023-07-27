8645f833fc269ca835f4f3c6ebbb6afc9859afb4
jeffvilla <jvilla125@gmail.com>
2023-07-27 13:55:09 -0700

    Remove static code from ProductDetailsPageComponent.js and import dynamic code from getProductList function

--- frontend/src/pages/ProductDetailsPage.js
+++ frontend/src/pages/ProductDetailsPage.js
@@ -2,15 +2,21 @@ import ProductDetailsPageComponent from "./components/ProductDetailsPageComponen
 import { useDispatch } from "react-redux";
 import { addToCart } from "../redux/actions/cartActions";
 
+import axios from "axios";
 
+const getProductDetails = async (id) => {
+    const { data } = await axios.get(`/api/products/get-one/${id}`);
+    return data;
+}
 
 const ProductDetailsPage = () => {
     const dispatch = useDispatch();
 
     return (
-        <ProductDetailsPageComponent 
-        addToCartReduxAction={addToCart}
-        reduxDispatch={dispatch}
+        <ProductDetailsPageComponent
+            addToCartReduxAction={addToCart}
+            reduxDispatch={dispatch}
+            getProductDetails={getProductDetails}
         />
     )
 };
