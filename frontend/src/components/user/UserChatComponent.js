0ca1313b7f4b34e7d60c7c4cd85a9617af1ee16e
jeffvilla <jvilla125@gmail.com>
2023-08-01 08:54:56 -0700

    Import audio to public folder. Create a new var for audio in UserChatComponent.js and have audio.play() function be called when a user receives a message from the admin

--- frontend/src/components/user/UserChatComponent.js
+++ frontend/src/components/user/UserChatComponent.js
@@ -16,6 +16,7 @@ const UserChatComponent = () => {
 
     useEffect(() => {
         if (!userInfo.isAdmin) {
+            var audio = new Audio("/audio/chat-msg.mp3");
             const socket = socketIOClient();
             setSocket(socket);
             socket.on("server sends message from admin to client", (msg) => {
@@ -23,6 +24,7 @@ const UserChatComponent = () => {
                     return [...chat, { admin: msg }];
                 })
                 setMessageReceived(true);
+                audio.play();
                 const chatMessages = document.querySelector(".cht-msg");
                 chatMessages.scrollTop = chatMessages.scrollHeight;
             })
